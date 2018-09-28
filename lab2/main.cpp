a2e99c5c9b700d5fe86dff02a28e146b40ec14c8
samdan <pri.samdan@gmail.com>
2018-09-28 15:09:34 +0800

    Patern

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -6,12 +6,12 @@ float point_color[3] = {1.0, 1.0, 1.0};
 
 void draw(int x, int y)
 {
-    glPointSize(point_size);
-    glColor3f(point_color[0], point_color[1], point_color[2]);
-    glBegin(GL_POINTS);
-    glVertex2f(x, y);
-    glEnd();
-    glFlush();
+	glPointSize(point_size);
+	glColor3f(point_color[0], point_color[1], point_color[2]);
+	glBegin(GL_POINTS);
+	glVertex2f(x, y);
+	glEnd();
+	glFlush();
 }
 
 void motion(int x, int y)
@@ -21,79 +21,78 @@ void motion(int x, int y)
 
 void mouse(int button, int state, int x, int y)
 {
-	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 		glutMotionFunc(motion);
 }
 
+void resize(int w, int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
-void resize(int w, int h) {
-	glMatrixMode   (GL_PROJECTION);
-	glLoadIdentity ();
+	glViewport(0, 0, w, h);
 
-   	glViewport     (0, 0, w, h);
+	gluOrtho2D(0, w, 0, h);
 
-	gluOrtho2D     (0, w, 0, h);
-
-	glMatrixMode   (GL_MODELVIEW);
-    glLoadIdentity();       
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-	switch(key)
+	switch (key)
 	{
 	case 'r':
-        point_color[0] = 1;
-        point_color[1] = 0;
-        point_color[2] = 0;
-        break;
+		point_color[0] = 1;
+		point_color[1] = 0;
+		point_color[2] = 0;
+		break;
 	case 'b':
-        point_color[0] = 0;
-        point_color[1] = 0;
-        point_color[2] = 1;
-        break;
+		point_color[0] = 0;
+		point_color[1] = 0;
+		point_color[2] = 1;
+		break;
 	case 'g':
-        point_color[0] = 0;
-        point_color[1] = 1;
-        point_color[2] = 0;
-        break;
+		point_color[0] = 0;
+		point_color[1] = 1;
+		point_color[2] = 0;
+		break;
 	case 'w':
-        point_color[0] = 1;
-        point_color[1] = 1;
-        point_color[2] = 1;
-        break;
-    case '1':
-        point_size = 1;
-        break;
-    case '2':
-        point_size = 2;
-        break;
-    case '3':
-        point_size = 3;
-        break;
-    case '4':
-        point_size = 4;
-        break;
-    case '5':
-        point_size = 5;
-        break;
-    default:
-        break;
+		point_color[0] = 1;
+		point_color[1] = 1;
+		point_color[2] = 1;
+		break;
+	case '1':
+		point_size = 1;
+		break;
+	case '2':
+		point_size = 2;
+		break;
+	case '3':
+		point_size = 3;
+		break;
+	case '4':
+		point_size = 4;
+		break;
+	case '5':
+		point_size = 5;
+		break;
+	default:
+		break;
 	}
 }
 
 int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
-    glutInitWindowSize(800, 600);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Paint");
-    glutReshapeFunc(resize);
-
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Paint");
+	glutReshapeFunc(resize);
 
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouse);
 
-    glutMainLoop();
+	glutMainLoop();
 }
